cea1429178f691c1793aa560605e84d2d4eeab30
Klaudia Czetyrow <czetyrow@gmail.com>
2026-03-29 15:11:20 +0200

    Dodano fetch data z coingecko w async

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react";
+const API_URL =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
 const App = () => {
-  return ( <div>Hello, World!</div> );
-}
- 
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL);
+        if (!res.ok) throw new Error("Failed to fetch data"); // jeśli odpowiedź serwera nie jest OK, zgłoś błąd
+        const data = await res.json(); // sparsuj (przetworzyć dane) odpowiedź jako JSON
+        console.log(data); // wyświetl pobrane dane w konsoli
+        setCoins(data); // zapisz dane o monetach w stanie komponentu
+      } catch (err) {
+        setError(err.message); // zapisz komunikat o błędzie w stanie
+      } finally {
+        setLoading(false); // zakończ tryb ładowania niezależnie od wyniku
+      }
+    }
+    fetchCoins(); // wywołaj funkcję pobierającą dane o monetach
+  }, []); // efekt uruchamiany tylko raz po pierwszym renderze komponentu
+
+  return (
+    <div>
+      <h1>🚀 Crypto Dash</h1>
+    </div>
+  );
+};
+
 export default App;
